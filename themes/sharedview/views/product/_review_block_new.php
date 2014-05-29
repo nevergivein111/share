92c9019eec23b0a66612531fdaf86205210501e4
Mohsin <nevergivein111@gmail.com>
2014-05-29 15:22:17 +0500

    no review

--- themes/sharedview/views/product/_review_block_new.php
+++ themes/sharedview/views/product/_review_block_new.php
@@ -1,6 +1,7 @@
 <?php echo CHtml::image(Yii::app()->createUrl('images/preload.gif'),'',array('style' => 'display:none','id' => 'loader_image'))?>
 
 <?php $reviews = $model->productReviewList();?>
+<?php if(empty($reviews)) { echo "<h2>No Reviews available yet</h2>";}?>
 <?php foreach($reviews as $key => $data):?>
         <?php $this->renderPartial("_review_list_new",array('data' => $data,'user' => $user,'key' => $key))?>
 <?php endforeach;?>
