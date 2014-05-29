07336d379297e0bec18881503424928b61728dde
amman <ammanwaheedmughal@gmail.com>
2014-05-29 15:52:09 +0500

    test

--- themes/sharedview/views/layouts/main.php
+++ themes/sharedview/views/layouts/main.php
@@ -5,7 +5,6 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="language" content="en"/>
-
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 
 	<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/fonts/fonts.css" media="all">
